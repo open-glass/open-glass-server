dd751e0b6ead107c9fc19faadf36845ac8b53ad7
Alexander Linder <alexander-linder@gmx.de>
2020-09-27 03:30:24 +0200

    Add a bunch more star citizen keybindings

--- open-glass-server/src/main/java/de/seine_eloquenz/open_glass/server/game/Games.kt
+++ open-glass-server/src/main/java/de/seine_eloquenz/open_glass/server/game/Games.kt
@@ -4,7 +4,7 @@ import java.awt.event.KeyEvent
 
 enum class Games(vararg keys: Int) : Game {
     //TODO Make these keybindings configurable in the future
-    //We're using default values for simplicities sake for now
+    //We're using default values for simplicity's sake for now
     NONE,
     STAR_CITIZEN(
             KeyEvent.VK_B, //Quantum
@@ -12,7 +12,12 @@ enum class Games(vararg keys: Int) : Game {
             KeyEvent.VK_G, //Gimbals
             KeyEvent.VK_P, //Weapons power
             KeyEvent.VK_O, //Shields power
-            KeyEvent.VK_I  //Engines power
+            KeyEvent.VK_I,  //Engines power
+            KeyEvent.VK_1, //Toggle lock pinned target
+            KeyEvent.VK_2, //Toggle lock pinned target
+            KeyEvent.VK_3, //Toggle lock pinned target
+            KeyEvent.VK_Y, //Leave Pilot seat / Eject (Hold)
+            KeyEvent.VK_BACK_SPACE // respawn/suicide / self destruct (Hold)
     ),
     ;
 
